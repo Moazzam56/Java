41ec43d27606dc48cf2c1144dc88b177a59f9058
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 09:06:27 -0700

    Update PatternNestedForLoopUserInput2.java

--- PatternNestedForLoopUserInput2.java
+++ PatternNestedForLoopUserInput2.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 
-public class TrianglePatternInput {
+public class import java.util.Scanner;
+
+public class PatternNestedForLoopUserInput2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
