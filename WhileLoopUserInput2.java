2449c9f6e3e529a48ce8e688d89d270e40f1b346
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 09:11:12 -0700

    Update WhileLoopUserInput2.java

--- WhileLoopUserInput2.java
+++ WhileLoopUserInput2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Palindrome {
+public class WhileLoopUserInput2 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
