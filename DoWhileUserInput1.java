6e08b1db42e7f525fb2cb2c78301c1f577b40e33
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 08:33:50 -0700

    Rename class from DigitCountSumDoWhile to DoWhileUserInput1

--- DoWhileUserInput1.java
+++ DoWhileUserInput1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DigitCountSumDoWhile {
+public class DoWhileUserInput1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
