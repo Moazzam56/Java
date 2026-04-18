be22c20e24b6de9960aee1550d7ed5e099a2b30d
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 08:54:01 -0700

    Update IfElseUserInput2.java

--- IfElseUserInput2.java
+++ IfElseUserInput2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LoginCheckSimple {
+public class IfElseUserInput2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
