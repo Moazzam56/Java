99cd1660e562e0803d64ce9fd37110b46035f686
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 08:42:36 -0700

    Update FactorialUserInputForLoop.java

--- FactorialUserInputForLoop.java
+++ FactorialUserInputForLoop.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Factorial {
+public class FactorialUserInputForLoop {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
