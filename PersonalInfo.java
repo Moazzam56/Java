1baa36f4581c1a88101739fd9edd920d6e93bd7a
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 08:11:10 -0700

    Create PersonalInfo.java

---
+++ PersonalInfo.java
@@ -0,0 +1,16 @@
+public class PersonalInfo {
+
+    public static void main(String[] args) {
+
+        // Variables
+        String name = "Your Name";
+        int age = 20;
+        String address = "Your Address";
+
+        // Output
+        System.out.println("----- Personal Information -----");
+        System.out.println("Name    : " + name);
+        System.out.println("Age     : " + age);
+        System.out.println("Address : " + address);
+    }
+}
