fb7473c30b1ad26355272cd45a4751e528d638d5
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 09:10:48 -0700

    Update WhileLoopUserInput1.java

--- WhileLoopUserInput1.java
+++ WhileLoopUserInput1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DigitSum {
+public class WhileLoopUserInput1 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
