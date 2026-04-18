18553424a909e27f1260d4862ae9801661715e4c
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 09:01:16 -0700

    Update NestedIfElseUserInput1.java

--- NestedIfElseUserInput1.java
+++ NestedIfElseUserInput1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StudentResult {
+public class NestedIfElseUserInput1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
