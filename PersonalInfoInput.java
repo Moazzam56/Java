2e8d2d1f160a9d6a3386b28f1f8aefc4a9903eaa
M.Moazzam <moazzam33466@gmail.com>
2026-04-18 08:06:26 -0700

    Create PersonalInfoInput.java

---
+++ PersonalInfoInput.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PersonalInfo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        System.out.print("Enter your address: ");
+        String address = sc.nextLine();
+
+        System.out.println("\n----- Personal Information -----");
+        System.out.println("Name    : " + name);
+        System.out.println("Age     : " + age);
+        System.out.println("Address : " + address);
+
+        sc.close();
+    }
+}
