fdc8abf590a8734938f06892ac6321f924b51312
M.Moazzam <moazzam33466@gmail.com>
2026-04-19 14:29:23 +0500

    Implement factorial calculation using nested loops
    Added logic to calculate factorials of numbers from 1 to 10.

--- FactorialSeries.java
+++ FactorialSeries.java
@@ -1,9 +1,12 @@
-public class FactorialUsingForLoop {
+public class FactorialSeries {
     public static void main(String[] args) {
 
+        // Outer loop: runs from 1 to 10
         for (int n = 1; n <= 10; n++) {
+
             long fact = 1;
 
+            // Inner loop: calculates factorial of current number n
             for (int i = 1; i <= n; i++) {
                 fact = fact * i;
             }
