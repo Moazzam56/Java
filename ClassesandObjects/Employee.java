a3cef4c3da8440c32cf3ee5d650cb525237d823d
M.Moazzam <moazzam33466@gmail.com>
2026-04-19 16:52:56 +0500

    Add Employee class with display method

---
+++ ClassesandObjects/Employee.java
@@ -0,0 +1,33 @@
+// Employee class
+
+class Employee {
+    String name;
+    double salary;
+
+    // Display employee details
+
+    void display() {
+        System.out.println(name);
+        System.out.println(salary);
+    }
+}
+
+// Main class 
+
+class Main {
+    public static void main(String[] args) {
+
+        // Creating Employee object
+
+        Employee T1 = new Employee();
+
+        // Assigning values
+
+        T1.name = "Abdullah";
+        T1.salary = 50000;
+
+        // Display output
+
+        T1.display();
+    }
+}
