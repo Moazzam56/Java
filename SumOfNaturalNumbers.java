82caa8f526a2a96a292b0930af0c522f85660a5a
M.Moazzam <moazzam33466@gmail.com>
2026-04-19 14:50:54 +0500

    Add comments to SumOfNaturalNumbers class
    Added comments to clarify the use of for loop.

--- SumOfNaturalNumbers.java
+++ SumOfNaturalNumbers.java
@@ -1,8 +1,10 @@
-public class ForLoopExample2 {
+public class SumOfNaturalNumbers {
     public static void main(String[] args) {
 
         int sum = 0;
-
+        
+        // using for loop
+        
         for (int i = 1; i <= 10; i++) {
             sum = sum + i;
         }
