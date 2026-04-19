a4cfc835fcbf09f698de35b9f5e5d43a84635180
M.Moazzam <moazzam33466@gmail.com>
2026-04-19 16:05:28 +0500

    Update and rename PatternNestedForLoopUserInput2.java to StarPatterns.java

--- StarPatterns.java
+++ StarPatterns.java
@@ -1,23 +1,8 @@
 import java.util.Scanner;
 
-public class import java.util.Scanner;
+// This program prints star patterns based on user input using nested for loops
 
-public class PatternNestedForLoopUserInput2 {
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter size: ");
-        int n = input.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-} {
+public class StarPatterns {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -25,11 +10,26 @@ public class PatternNestedForLoopUserInput2 {
         System.out.print("Enter rows: ");
         int n = input.nextInt();
 
+        // Square pattern
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Right triangle pattern
+        
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
+        input.close();
     }
 }
