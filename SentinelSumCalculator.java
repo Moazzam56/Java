db5403be0700d5378eade1a00c525b27f6e1f16d
M.Moazzam <moazzam33466@gmail.com>
2026-04-19 14:20:21 +0500

    Update and rename DoWhileUserInput2.java to SentinelSumCalculator.java

--- SentinelSumCalculator.java
+++ SentinelSumCalculator.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
-public class DoWhileUserInput2 {
+public class SentinelSumCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int num;
         int sum = 0;
-
+        
+        // using do-while loop
+        
         do {
             System.out.print("Enter number (0 to stop): ");
             num = sc.nextInt();
 
-            sum = sum + num;
+            sum = sum + num; 
 
         } while (num != 0);
 
