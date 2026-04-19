5b08a6ceb2f443ca8bb3653f00d3dcb164d16a61
M.Moazzam <moazzam33466@gmail.com>
2026-04-19 17:49:43 +0500

    Implement display method in Circle class
    Added display method to calculate and print area and circumference.

---
+++ ClassesandObjects/TakingInputFromUser/Circle.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// Circle class
+
+class Circle {
+    double radius;
+
+    // Display area and circumference
+  
+    void display() {
+        System.out.println("Area: " + (3.14 * radius * radius));
+        System.out.println("Circumference: " + (2 * 3.14 * radius));
+    }
+}
+
+// Main class
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Circle c1 = new Circle();
+
+        System.out.println("Enter radius:");
+        c1.radius = sc.nextDouble();
+
+        c1.display();
+
+        sc.close();
+    }
+}
