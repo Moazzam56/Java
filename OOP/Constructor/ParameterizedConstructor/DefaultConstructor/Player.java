07982bd9aec2498dfdfd068eb9cf52e000131aee
M.Moazzam <moazzam33466@gmail.com>
2026-04-21 12:30:34 -0700

    Add default constructor to Player class
    Updated comments for clarity and added a default constructor that prints a message when a new player is created.

---
+++ OOP/Constructor/ParameterizedConstructor/DefaultConstructor/Player.java
@@ -0,0 +1,17 @@
+class Player {
+
+    // Default Constructor
+  
+    Player() {
+        System.out.println("New Player Joined the Game");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating multiple objects
+      
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
+    }
+}
